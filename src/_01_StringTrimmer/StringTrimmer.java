e431cf1b1070450613348e76760a6c0d9f55ca80
league <league@imac-109.attlocal.net>
2022-10-02 11:26:28 -0700

    recursion

--- src/_01_StringTrimmer/StringTrimmer.java
+++ src/_01_StringTrimmer/StringTrimmer.java
@@ -2,25 +2,25 @@ package _01_StringTrimmer;
 
 public class StringTrimmer {
 
-    /*
-     * This method removes the specified number of characters from the right
-     * side of the specified string and returns the trimmed string. 
-     */
-    static public String trimString(String str, int removesLeft) {
-        // 1. If there are no removes left
-            if (removesLeft == 0) {
-				
-            
-            // 2. Return the string
-            return str;
-        // 3. Else you need to remove the last letter from the string,
-        //    reduce removesLeft by 1, and then call trimString() 
-            }else {
-          
-            	removesLeft = removesLeft-1;
-            	trimString(str, removesLeft);
-            	return null;
-            }
-       
-    }
+	/*
+	 * This method removes the specified number of characters from the right side of
+	 * the specified string and returns the trimmed string.
+	 */
+	static public String trimString(String str, int removesLeft) {
+		// 1. If there are no removes left
+		if (removesLeft == 0) {
+
+			// 2. Return the string
+			return str;
+			// 3. Else you need to remove the last letter from the string,
+			// reduce removesLeft by 1, and then call trimString()
+		}else {
+		
+			removesLeft = removesLeft - 1;
+			return trimString(str.substring(0, str.length()-1), removesLeft);
+			
+		}
+
+	}
 }
+
--- src/_02_RecursionMath/RecursionMathTest.java
+++ src/_02_RecursionMath/RecursionMathTest.java
@@ -18,12 +18,12 @@ class RecursionMathTest {
     @Test
     public void testDivision() {
         // Add JUnit tests to test your method
-        fail("Write some tests!");
+        assertEquals(20, RecursionMath.recursiveDivision(100, 5));
     }
 
     @Test 
     public void testPower() {
         // Add JUnit tests to test your method
-        fail("Write some tests!");
+        assertEquals(625, RecursionMath.recursivePower(5, 4));
     }
 }
